6e67d8d973a8b18a296c9e61b4e21f00f577337a
Manuel Kour <manuelkour@gmail.com>
2021-05-01 10:23:07 +0200

    First commit. Hab meinen alten JDA Bot in Kotlin mit Kord umgeschrieben

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+group = "me.manue"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit"))
+    implementation("dev.kord:kord-core:0.7.0-RC3")
+    implementation ("khttp:khttp:1.0.0")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/Functionalities.kt
@@ -0,0 +1,38 @@
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.Kord
+import dev.kord.core.entity.channel.MessageChannel
+import khttp.get as httpGet
+import khttp.post as httpPost
+
+var current_video: String = "https://www.youtube.com/watch?v=wSsEvCxiijs"
+
+fun w2g(video: String) : String {
+    val response = httpPost(
+        url = "https://w2g.tv/rooms/create.json",
+        json = mapOf("w2g_api_key" to "i37lhu79syq2diq7pr335fwiv8uvdbb4zs3e6gj3uhawqokt1d1pm1n6fh5a2sg2",
+            "share" to video, "bg_color" to "#000000", "bg_opacity" to "50"))
+
+    return response.text.split(",")[1]
+                .substring(12).replace("\"","")
+}
+
+suspend fun youtube(bot: Kord) {
+    // Thread.sleep(900000)
+    try {
+        val response = httpGet("https://www.googleapis.com/youtube/v3/search?key=AIzaSyAYd0qg6KQuEy-Kl6e5ILAjNTu2ZWT0TxU&channelId=UCun2qqzZOOVcuW9hZhaDxmQ&part=snippet,id&order=date&maxResults=1")
+        val video = response.text.split(",")[3]
+        for (s in response.text.split(","))
+            println(s)
+        println(response.text.split(",").size)
+
+        val channel : MessageChannel
+        channel = bot.getChannel(Snowflake(166884795555643392))?.asChannel() as MessageChannel
+        if(!video.equals(current_video)) {
+            current_video = video
+            channel.createMessage("@everyone Neues Video ist draußen! https://www.youtube.com/watch?v=" + current_video)
+        }
+    } catch (e: Exception) {
+        println("Beim API Call ist etwas falsch gelaufen \n$e")
+    }
+
+}
